8032b37ba267161a4f60b56c96047f184f01de05
anantarora <anant_arora19@yahoo.com>
2020-03-01 00:17:04 +0530

    Gather assess clean

--- DataModellingPostgres/src/main/python/cassandra.py
+++ DataModellingPostgres/src/main/python/cassandra.py
@@ -32,4 +32,44 @@ except Exception as e:
 
 query = "INSERT INTO songs (song_title, artist_name, year, album_name, single) "
 query = query + "values (%s,%s,%s,%s,%s)"
+try:
+    session.execute (query,("Across The Universe", "The Beatles", 1970, "Let It Be",False))
+except Exception as e:
+    print('Error setting up keyspace')
+    print(e)
 
+
+query = "INSERT INTO songs (song_title, artist_name, year, album_name, single) "
+query = query + "values (%s,%s,%s,%s,%s)"
+try:
+    session.execute (query,("Think For Yourself","The Beatles", 1965, "Rubber Soul",False))
+except Exception as e:
+    print('Error setting up keyspace')
+    print(e)
+
+
+query = "SELECT * from songs "
+
+try:
+    rows=session.execute (query)
+except Exception as e:
+    print('Error setting up keyspace')
+    print(e)
+
+for i in rows:
+    print (i.year, i.artist_name, i.album_name)
+
+query = "SELECT * from songs "
+query = query + "where year = 1970 and artist_name ='The Beatles'"
+try:
+    rows=session.execute (query)
+except Exception as e:
+    print('Error setting up keyspace')
+    print(e)
+
+for i in rows:
+    print(i.year, i.artist_name,i.album_name)
+
+
+session.shutdown()
+conn.shutdown()
