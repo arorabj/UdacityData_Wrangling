913fad10b346ac92147016a9be7a1df8c2d836b0
anantarora <anant_arora19@yahoo.com>
2020-03-11 01:01:55 +0530

    Gather assess clean

--- SongAnalysis/src/main/python/etl.py
+++ SongAnalysis/src/main/python/etl.py
@@ -1,11 +1,59 @@
 import os
-import glob
 import configparser as cp
+import pandas as pd
+import json
+import psycopg2
+from SongAnalysis.src.main.python.sql_queries import *
 
-env='dev'
-conf=cp.RawConfigParser()
-conf.read('../../../resources/application.properties')
-filepath = ''
-#def process_data(cur, conn, filepath):
 
-log_files =  glob(filepath,'*.')
+def process_data(cur, conn, filepath, func):
+    for root, dirname, filename in os.walk(filepath):
+        for files in filename:
+            if '.json' in files:
+                func(cur, conn, os.path.join(root,files))
+
+
+def read_song_data(cur, conn, filepath):
+    try:
+        with open (filepath) as myfile:
+            myfiledata= myfile.read()
+            jsonDict = json.loads(myfiledata)
+            df = pd.DataFrame([jsonDict])
+            #print (df)
+    except Exception as e:
+        print (e)
+
+    song_data = [df['song_id'].values,df['title'].values,df['artist_id'].values,df['year'].values,df['duration'].values]
+    #cur.execute(songs_insert,song_data)
+
+    artist_data = [df['artist_id'].values, df['name'].values, df['location'].values, df['latitude'].values,df['longitude'].values]
+    #cur.execute(artists_insert,song_data)
+    conn.commit()
+
+def main():
+    env = 'dev'
+    conf = cp.RawConfigParser()
+    conf.read('../../../resources/application.properties')
+    log_files_dir = conf.get(env, 'LogData')
+    song_files_dir = conf.get(env, 'SongData')
+
+    hostname = conf.get(env, 'DBhost')
+    dbname = conf.get(env, 'DBName')
+    username = conf.get(env, 'UserName')
+    password = conf.get(env, 'Password')
+    connect_str = 'host=' + hostname + ' dbname=sparkifydb user=' + username + ' password=' + password
+
+    conn = psycopg2.connect(connect_str)
+    cur = conn.cursor()
+
+    pd.set_option('display.width', 7000)
+    pd.set_option('display.max_columns', 10)
+
+    # process_data(log_files_dir)
+    process_data(conn, cur, song_files_dir, read_song_data)
+
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
