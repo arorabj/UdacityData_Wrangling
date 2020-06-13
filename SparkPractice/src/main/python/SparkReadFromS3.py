973c66abedecb01bdab0df3cbf63dfb36e566b55
anantarora <anant_arora19@yahoo.com>
2020-06-14 00:02:03 +0530

    Gather assess clean

--- SparkPractice/src/main/python/SparkReadFromS3.py
+++ SparkPractice/src/main/python/SparkReadFromS3.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 import os
 import configparser as cp
 import pyspark.sql.functions as F
+from pyspark.sql.types import *
 env="AWS"
 confPath="../../../resources/application.properties"
 conf =  cp.RawConfigParser()
@@ -12,11 +13,11 @@ os.environ['AWS_ACCESS_KEY']=conf.get(env,'AWS_SECRET')
 
 spark = SparkSession.builder.config("spark.jars.packages","org.apache.hadoop:hadoop-aws:2.7.0").getOrCreate()
 
-df = spark.read.csv("s3a://")
+df = spark.read.csv("s3a://udacity-dend/pagila/payment/payment.csv")
 print(df.printSchema())
 print(df.show(5))
 
-df = spark.read.csv("s3a://",sep=';',inferSchema=True,header=True)
+df = spark.read.csv("s3a://udacity-dend/pagila/payment/payment.csv",sep=';',inferSchema=True,header=True)
 print(df.printSchema())
 print(df.show(5))
 
@@ -25,4 +26,24 @@ df_payment = df.withColumn("payment_date", F.to_timestamp('payment_date'))
 print(df_payment.printSchema())
 print(df_payment.show(5))
 
+df_payment = df_payment.withColumn("month", F.month('payment_date'))
+print(df_payment.show(5))
 
+df_payment.createOrReplaceTempView("payment")
+print(spark.sql("select month, sum(amount) as revenue from payment group by month order by revenue desc").show())
+
+
+#fix schema
+paymentSchema = StructType([
+    StructField("payment_id",IntegerType()),
+    StructField("customer_id",IntegerType()),
+    StructField("staff_id",IntegerType()),
+    StructField("rental_id",IntegerType()),
+    StructField("amount",DoubleType()),
+    StructField("payment_date",DateType())
+])
+
+df = spark.read.csv("s3a://udacity-dend/pagila/payment/payment.csv",sep=';',schema=paymentSchema,header=True)
+
+df_payment.createOrReplaceTempView("payment")
+print(spark.sql("select month(payment_date) as mnth, sum(amount) as revenue from payment group by mnth order by revenue desc").show())
